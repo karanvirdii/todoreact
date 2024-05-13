f85ec5fb6c50cffd93e3dc78fcb8d50a772ff907
KaranVirdiRMIT <s4094364@student.rmit.edu.au>
2024-05-13 15:20:09 +1000

    first commit

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "First todo",
+        completed: false
+    },
+    {
+        id: 2,
+        title: "Second todo",
+        completed: false
+    },
+    {
+        id: 3,
+        title: "Third todo",
+        completed: false
+    }
+]
